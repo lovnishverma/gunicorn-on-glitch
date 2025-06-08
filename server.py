8bea850387eb70ff28c5b3ed2fb5a33417e6df53
Glitch (flasktestapp) <none>
2025-06-08 07:37:02 +0000

    🍪☕️ Checkpoint ./server.py:79936297/2 ./glitch.json:79936297/729 ./requirements.txt:79936297/41

--- server.py
+++ server.py
@@ -20,4 +20,4 @@ def lookup(code):
     return jsonify({"error": "Code expired or not found"}), 404
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=3000)
